b4555356c5b5ae3508d379e34ed3846c22b24da5
willybnz <binazoneddie@gmail.com>
2025-01-16 12:31:11 +0100

    adding functions

--- BASIC_FUNCTION_IN_C/concat_params.c
+++ BASIC_FUNCTION_IN_C/concat_params.c
@@ -14,23 +14,22 @@ char *concat_params(int ac, char **av)
 {
     int i = 0, j = 0, k = 0, len = 0;
 
-    for (; av[i] != NULL; i++) {
+    for (; i < ac; i++) {
         len = len + my_strlen(av[i]);
-    } i = 0, len = len + ac + 1;
+    } i = 0, len = len + (ac - 1) + 1;
 
     char *stock = malloc(sizeof(char) * (len));
 
-    for(; i < ac ; i++, j = 0) {
-        for (; av[i][j] != '\0'; j++, k++) {
-            stock[k] = av[i][j];
-        } stock[k] = '\n';
-    }    
-    return stock;
-}
+    if (stock == NULL) {
+        return NULL;
+    }
 
-int main(int ac, char **av)
-{
-    char *test = concat_params(ac, av);
-    printf("%s", test);
-    free(test);
+    while (i < ac) {
+        while (av[i][j] != '\0') {
+            stock[k] = av[i][j];
+            j++, k++;
+        } stock[k] = '\n';
+        i++, j = 0, k++;
+    } stock[k] = '\0';
+    return stock;
 }
